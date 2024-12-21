d6e8cabcf0e511a0f357171234baed31872ca2a9
reecetec <reece.tec@gmail.com>
2024-12-21 10:09:59 -0500

    train model

---
+++ train_model.py
@@ -0,0 +1,13 @@
+import mlflow
+
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_diabetes
+from sklearn.ensemble import RandomForestRegressor
+
+mlflow.sklearn.autolog()
+
+db = load_diabetes()
+X_train, X_test, y_train, y_test = train_test_split(db.data, db.target)
+
+rf = RandomForestRegressor(n_estimators=100, max_depth=6, max_features=3)
+rf.fit(X_train, y_train)
